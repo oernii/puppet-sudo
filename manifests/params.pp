5681007c62abcc161f47f7c21a77fb5d7eb01cf0
oernii <oernii@gmail.com>
2012-04-20 15:12:23 +0200

    fedora

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 class sudo::params {
   case $::operatingsystem {
-    ubuntu, debian, redhat, mandriva, centos: {
+    ubuntu, debian, redhat, mandriva, centos, fedora: {
       $package = 'sudo'
       $config_file = '/etc/sudoers'
       $config_dir = '/etc/sudoers.d/'
